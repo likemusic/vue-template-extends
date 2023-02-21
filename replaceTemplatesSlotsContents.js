063d9372c2ce2fcba4bfd5f2e73bd7b23e17733f
Valerij Ivashchenko <likemusicdev@gmail.com>
2023-02-21 21:06:35 +0300

    Fix nested <template> processing.

--- replaceTemplatesSlotsContents.js
+++ replaceTemplatesSlotsContents.js
@@ -4,8 +4,24 @@ module.exports = function (fragment, slotsContents, replaceSlotsContent) {
 
     for (let i = 0; i < templatesLength; i++) {
         let template = templates.item(i);
+
+        // ignoe nested templates
+        if (isNestedTemplate(templates, i, template)) {
+            continue;
+        }
         // let templateInnerHtml = template.innerHTML;
 
-        template.innerHTML = replaceSlotsContent(template, slotsContents);
+        // template.innerHTML = replaceSlotsContent(template, slotsContents);
+        replaceSlotsContent(template, slotsContents);
     }
+}
+
+function isNestedTemplate(templates, currentIndex, template) {
+    for (let i = 0; i < currentIndex; i++) {
+        if (templates[i].contains(template)) {
+            return true;
+        }
+    }
+
+    return false;
 }
