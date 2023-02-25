85f0fa0807ba2740bd8ec854cb92531dc2b83637
Valery Ivashchanka <likemusicdev@gmail.com>
2023-02-26 01:43:30 +0300

    Fix nested slots overriding in child templates.

--- getSlotsContents.js
+++ getSlotsContents.js
@@ -5,31 +5,43 @@ module.exports = function (extendsContent, baseSlotsData = {}) {
     // debugger;
     const {document} = parseHTML(extendsContent);
 
-    const children = document.children;
-    const childrenLength = children.length;
+    // const children = document.children;
+    // const childrenLength = children.length;
+
+    const slots = document.querySelectorAll('slot');
+    const slotsLength = slots.length;
 
     const ret = {};
 
-    for (let i = 0; i < childrenLength; i++) {
-        let child = children.item(i);
-        let childNodeName = child.nodeName;
+    for (let i = 0; i < slotsLength; i++) {
+        let slot = slots[i];
+        // let slotName = slot.nodeName;
 
-        if (childNodeName !== 'SLOT') {
-            throw new Error('`extends` block must contain only `slot` nodes! Given: ' + childNodeName);
-        }
+        // if (slotName !== 'SLOT') {
+        //     throw new Error('`extends` block must contain only `slot` nodes! Given: ' + slotName);
+        // }
 
-        let childAttributes = child.attributes;
+        let slotAttributes = slot.attributes;
 
         // name
-        let nameAttribute = childAttributes.getNamedItem('name');
+        let nameAttribute = slotAttributes.getNamedItem('name');
         let slotName = nameAttribute ? nameAttribute.value : defaultSlotName;
 
         // replace
-        let replaceAttribute = childAttributes.getNamedItem('replace');
+        let replaceAttribute = slotAttributes.getNamedItem('replace');
         let isReplace = Boolean(replaceAttribute && (replaceAttribute.value !== 'false'));
 
+        // parent
+        let parentAttribute = slotAttributes.getNamedItem('parent');
+        let isParent = Boolean(parentAttribute && (parentAttribute.value !== 'false'));
+
+        // ignore parent slots
+        if (isParent) {
+            continue;
+        }
+
         ret[slotName] = {
-            content: child.innerHTML,
+            content: slot.innerHTML,
             isReplace: isReplace,
         };
     }
--- replace/replaceSlotsContent.js
+++ replace/replaceSlotsContent.js
@@ -3,10 +3,11 @@ const constants = require('../defaults');
 
 function replaceSlotsContent(document, slotsData) {
     const slots = document.querySelectorAll('slot');
-    const slotsLength = slots.length;
+    const topLevelSlots = filterTopLevelSlots(slots);
+    const slotsLength = topLevelSlots.length;
 
     for (let i = 0; i < slotsLength; i++) {
-        let slot = slots.item(i);
+        let slot = topLevelSlots[i];
 
         let slotAttributes = slot.attributes;
 
@@ -27,9 +28,35 @@ function replaceSlotsContent(document, slotsData) {
         }
 
         slot.innerHTML = slotData.content;
+
+        replaceSlotsContent(slot, slotsData);
     }
 
     replaceTemplatesSlotsContents(document, slotsData, replaceSlotsContent);
+}
+
+function isNestedSlot(slots, currentIndex, slot) {
+    for (let i = 0; i < currentIndex; i++) {
+        if (slots[i].contains(slot)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function filterTopLevelSlots(slots) {
+    const topLevelSlots = [];
+
+    for (let i = 0; i < slots.length; i++) {
+        let slot = slots[i];
+
+        if (!isNestedSlot(slots, i, slot)) {
+            topLevelSlots.push(slot);
+        }
+    }
+
+    return topLevelSlots;
 }
 
 module.exports = replaceSlotsContent;
